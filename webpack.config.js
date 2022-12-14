4c8859f368571d46565df56b8db2f51c3e3ea647
llallonen <llallonen@gmail.com>
2022-12-15 01:17:21 +0400

    chore: add start webpack settings

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: path.resolve(__dirname, "./src/index.html"),
+  mode: 'development',
+  devServer: {
+    static: './public',
+  },
+  module: {
+    rules: [ {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(s[ac]|c)ss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.ts$/i,
+        use: "ts-loader",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "./public"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "style.css",
+    }),
+    // new CleanWebpackPlugin({
+    //   cleanOnceBeforeBuildPatterns: ["**/*", "!.git"],
+    // }),
+  ],
+};
