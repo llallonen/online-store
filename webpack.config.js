60eaf89d103a8ff360883cb9e8715abe7b4993cf
llallonen <llallonen@gmail.com>
2022-12-15 16:46:58 +0400

    fix: fix the entry point for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const EslintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
-    entry: path.resolve(__dirname, './src/index.html'),
+    entry: path.resolve(__dirname, './src/index'),
     mode: 'development',
     devServer: {
         static: './public',
