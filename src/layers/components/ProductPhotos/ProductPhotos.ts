432e36fb3d3fb788d48c654b3283a6abe9292aef
llallonen <llallonen@gmail.com>
2022-12-23 21:25:02 +0400

    feat: implement ProductPhoto component

---
+++ src/layers/components/ProductPhotos/ProductPhotos.ts
@@ -0,0 +1,39 @@
+import Observer from '../../Observer/Observer';
+import data from '../../../data.json';
+import { IProductPhotos } from './ProductPhotos.types';
+import './ProductPhotos.scss';
+
+class ProductPhotos {
+    private container: HTMLElement;
+    private observer: Observer;
+
+    constructor({ container, observer }: IProductPhotos) {
+        this.container = container;
+        this.observer = observer;
+    }
+
+    public render() {
+        const productPhotos = document.createElement('div');
+        productPhotos.classList.add('product-card__photos');
+        this.container.append(productPhotos);
+
+        const productSlider = document.createElement('div');
+        productSlider.classList.add('product-card__slider');
+        productPhotos.append(productSlider);
+
+        const productSlides = data.products[1].images;
+        productSlides.map((item) => {
+            const slide = document.createElement('img');
+            slide.classList.add('product-card__slide');
+            slide.src = `${item}`;
+            productSlider.append(slide);
+        });
+
+        const productThumbnail = document.createElement('img');
+        productThumbnail.classList.add('product-card__thumbnail');
+        productThumbnail.src = `${data.products[1].images[0]}`;
+        productPhotos.append(productThumbnail);
+    }
+}
+
+export { ProductPhotos };
---
+++ src/layers/components/ProductPhotos/ProductPhotos.types.ts
@@ -0,0 +1,8 @@
+import Observer from '../../Observer/Observer';
+
+interface IProductPhotos {
+    container: HTMLElement;
+    observer: Observer;
+}
+
+export { IProductPhotos };
