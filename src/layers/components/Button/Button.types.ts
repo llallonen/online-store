42a1f24f9ff45954dd91634636006fcac2b3cf54
Andrey Lapin <nethack@mail.ru>
2022-12-28 16:42:01 +0400

    fix: add event option for button

--- src/layers/components/Button/Button.types.ts
+++ src/layers/components/Button/Button.types.ts
@@ -1,8 +1,10 @@
 import Observer from '../../Observer/Observer';
+import { EventName } from '../../Observer/Observer.types';
 
 interface IButtonProps {
     container: HTMLElement;
     observer: Observer;
+    event?: EventName;
 }
 
 export { IButtonProps };
