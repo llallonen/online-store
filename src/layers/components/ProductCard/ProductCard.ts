a6c20ae0fe4b64feae7ae1ca0901e138e866a2eb
Andrey Lapin <nethack@mail.ru>
2023-01-07 16:29:35 +0400

    fix: invalid product query

--- src/layers/components/ProductCard/ProductCard.ts
+++ src/layers/components/ProductCard/ProductCard.ts
@@ -29,86 +29,93 @@ class ProductCard {
         productCard.classList.add('product');
         this.container.append(productCard);
 
-        const productInfo = document.createElement('div');
-        productInfo.classList.add('product__info');
-        productCard.append(productInfo);
-
-        let productPhotos;
-        let productDescription;
-        let addButton;
-        let buyNowButton;
-        let removeButton;
-
         const productData = this.data.goods.products.find((el) => el.id === this.data.currProduct.id);
-        if (productCard instanceof HTMLElement) {
-            if (productData) {
-                productPhotos = new ProductPhotos({
+
+        if (productData) {
+            const productInfo = document.createElement('div');
+            productInfo.classList.add('product__info');
+            productCard.append(productInfo);
+
+            let productPhotos;
+            let productDescription;
+            let addButton;
+            let buyNowButton;
+            let removeButton;
+
+            if (productCard instanceof HTMLElement) {
+                if (productData) {
+                    productPhotos = new ProductPhotos({
+                        container: productCard,
+                        observer: this.observer,
+                        currImg: this.currImg,
+                        data: this.data,
+                    });
+
+                    productDescription = new ProductDescription({
+                        container: productInfo,
+                        observer: this.observer,
+                        product: productData,
+                    });
+                }
+
+                const productBtns = document.createElement('div');
+                productBtns.classList.add('product__btns');
+                productInfo.append(productBtns);
+
+                if (this.data.basket.products.find((el) => el.id === this.data.currProduct.id)) {
+                    removeButton = new Button({
+                        container: productBtns,
+                        observer: this.observer,
+                        typeButton: 'button--product',
+                        textButton: 'Drop from cart',
+                        id: this.data.currProduct.id,
+                        event: EventName.removeGoods,
+                    });
+                } else {
+                    addButton = new Button({
+                        container: productBtns,
+                        observer: this.observer,
+                        typeButton: 'button--product',
+                        textButton: 'Add to cart',
+                        id: this.data.currProduct.id,
+                        event: EventName.addGoods,
+                    });
+                }
+                buyNowButton = new Button({
+                    container: productBtns,
+                    observer: this.observer,
+                    typeButton: 'button--product',
+                    textButton: 'Buy now',
+                });
+            }
+            if (productCard && productData) {
+                new Breadcrumbs({
                     container: productCard,
                     observer: this.observer,
-                    currImg: this.currImg,
-                    data: this.data,
-                });
-
-                productDescription = new ProductDescription({
-                    container: productInfo,
-                    observer: this.observer,
+                    breadData: this.data,
                     product: productData,
-                });
+                }).render();
             }
-
-            const productBtns = document.createElement('div');
-            productBtns.classList.add('product__btns');
-            productInfo.append(productBtns);
-
-            if (this.data.basket.products.find((el) => el.id === this.data.currProduct.id)) {
-                removeButton = new Button({
-                    container: productBtns,
-                    observer: this.observer,
-                    typeButton: 'button--product',
-                    textButton: 'Drop from cart',
-                    id: this.data.currProduct.id,
-                    event: EventName.removeGoods,
-                });
-            } else {
-                addButton = new Button({
-                    container: productBtns,
-                    observer: this.observer,
-                    typeButton: 'button--product',
-                    textButton: 'Add to cart',
-                    id: this.data.currProduct.id,
-                    event: EventName.addGoods,
-                });
+            if (productDescription) {
+                productDescription.render();
             }
-            buyNowButton = new Button({
-                container: productBtns,
-                observer: this.observer,
-                typeButton: 'button--product',
-                textButton: 'Buy now',
-            });
-        }
-        if (productCard && productData) {
-            new Breadcrumbs({
-                container: productCard,
-                observer: this.observer,
-                breadData: this.data,
-                product: productData,
-            }).render();
-        }
-        if (productDescription) {
-            productDescription.render();
-        }
-        if (productPhotos) {
-            productPhotos.render();
-            productPhotos.renderThumbnail();
-        }
-        if (addButton) {
-            addButton.render();
-        }
-        if (removeButton) {
-            removeButton.render();
-        }
-        if (buyNowButton) {
-            buyNowButton.render();
+            if (productPhotos) {
+                productPhotos.render();
+                productPhotos.renderThumbnail();
+            }
+            if (addButton) {
+                addButton.render();
+            }
+            if (removeButton) {
+                removeButton.render();
+            }
+            if (buyNowButton) {
+                buyNowButton.render();
+            }
+        } else {
+            const header = document.createElement('h2');
+            header.textContent = 'Product not found';
+            productCard.append(header);
         }
     }
 }
